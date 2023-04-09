97d7b74d79bec65dbbb9bddb56681a407606a83c
TamilKannanCV <tamilkannan.github@gmail.com>
2023-04-09 09:35:23 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/vioai/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vioai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
